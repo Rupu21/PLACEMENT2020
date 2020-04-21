8ee8316e7b5fdbae2504890000e9f5b0a6655617
Rupu21 <57573869+Rupu21@users.noreply.github.com>
2020-04-22 01:39:08 +0530

    Add files via upload

---
+++ LeetCode/10RegularExpression.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+       if(p.length()>0 && p.charAt(0)=='*')
+           return false;
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i-1]) {
+                dp[0][i+1] = true;
+            }
+        }
+        for (int i = 0 ; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.' || (p.charAt(j) == s.charAt(i))) {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                else if (p.charAt(j) == '*') {
+                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
+                        dp[i+1][j+1] = dp[i+1][j-1];
+                    } else {
+                        dp[i+1][j+1] = (dp[i][j+1] || dp[i+1][j-1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
